7bc817cdc40718adbd9e69788e24765d54584910
Kent <>
2026-08-11 18:33:27 +0800

    .

--- g2c-s/index.4ff28.js
+++ g2c-s/index.4ff28.js
@@ -1,4 +1,4 @@
-System.register(["./application.25600.js"], function (_export, _context) {
+System.register(["./application.82571.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
