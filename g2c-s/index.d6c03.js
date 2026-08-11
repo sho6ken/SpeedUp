1592210697d8e37c0fcd8fa6cd8c1f22daa3ce5b
Kent <>
2026-08-11 11:56:14 +0800

    .

--- g2c-s/index.d6c03.js
+++ g2c-s/index.d6c03.js
@@ -1,4 +1,4 @@
-System.register(["./application.a33bf.js"], function (_export, _context) {
+System.register(["./application.aafce.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
