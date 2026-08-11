82e991aa4d9a6d09e4b87e292276ba99234ea6b0
Kent <>
2026-08-11 15:22:04 +0800

    .

--- g2c-s/application.e2202.js
+++ g2c-s/application.e2202.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.f658e.json';
+          this.settingsPath = 'src/settings.6941b.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
--- g2c-s/index.f0cab.js
+++ g2c-s/index.f0cab.js
@@ -1,4 +1,4 @@
-System.register(["./application.9e400.js"], function (_export, _context) {
+System.register(["./application.e2202.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
