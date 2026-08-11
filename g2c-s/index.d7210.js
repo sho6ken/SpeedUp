35fde1066d244c77a1ce6a469fae331e2329280d
Kent <>
2026-08-11 12:54:59 +0800

    .

--- g2c-s/index.d7210.js
+++ g2c-s/index.d7210.js
@@ -1,4 +1,4 @@
-System.register(["./application.75682.js"], function (_export, _context) {
+System.register(["./application.c8f39.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
