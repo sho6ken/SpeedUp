5be6d7de6f498831d7fcb979cf0563166eaa05f4
Kent <>
2026-08-14 15:12:11 +0800

    .

--- g3c-s/index.eec1b.js
+++ g3c-s/index.eec1b.js
@@ -1,4 +1,4 @@
-System.register(["./application.0c758.js"], function (_export, _context) {
+System.register(["./application.4c3e0.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
