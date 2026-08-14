137104776453abf90878f761781d7de1ea0b28fc
Kent <>
2026-08-14 14:54:21 +0800

    .

--- g3c-s/application.9342f.js
+++ g3c-s/application.9342f.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.ec9f4.json';
+          this.settingsPath = 'src/settings.6b2e7.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
--- g3c-s/index.3bc60.js
+++ g3c-s/index.3bc60.js
@@ -1,4 +1,4 @@
-System.register(["./application.1f8e2.js"], function (_export, _context) {
+System.register(["./application.9342f.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
